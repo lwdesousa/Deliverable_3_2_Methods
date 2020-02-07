ae1ab0e386c9b8d2ebb634d6b8a135ae14846126
Leticia Witkowsky De Sousa <lwdesousa@usf.edu>
2020-02-07 17:51:39 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿/* Author:Leticia W. de Sousa
+ * Date:02/04/2020
+ * Description:
+ */
+    using System;
+
+namespace Deliverable_3_2_Methods
+{
+    class Program
+       
+    {
+        static void Main(string[] args)
+        {
+      
+            try
+            {
+                //Ask the user to write a input (name)
+                Console.WriteLine("Please,enter your name:");
+                string userName = HelloName();
+
+                //Show messsage to user
+                Console.WriteLine("Hello " + userName + "!");
+            }
+
+                catch
+            {
+                //If error in the code, show the following message
+                Console.WriteLine("An error was present in the code. Please try again. ");
+
+            }
+        }
+
+
+        private static string HelloName()
+        {
+            string UserName = Console.ReadLine();
+            return UserName;
+        }
+    }
+}
